22ea18144f5e4efec799bb3366e99c85795d815e
Akshay Easwaran <akeaswaran@me.com>
2018-05-31 12:45:23 -0700

    fixed typo

--- mls-bar/View Controllers/MatchViewController.m
+++ mls-bar/View Controllers/MatchViewController.m
@@ -201,7 +201,7 @@
                         NSUserNotification *userNote = [[NSUserNotification alloc] init];
                         userNote.identifier = [NSString stringWithFormat:@"away-score-%@", self.selectedGame.gameId];
                         userNote.title = [NSString stringWithFormat:@"⚽ %@ GOAL", self.selectedGame.awayCompetitor.team.abbreviation];
-                        NSArray *goals = [[self->matchEvents filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF.type = goal"] sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+                        NSArray *goals = [[self->matchEvents filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF.type = goal"]] sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
                             NSDictionary *objA = (NSDictionary *)obj1;
                             NSDictionary *objB = (NSDictionary *)obj2;
                             
