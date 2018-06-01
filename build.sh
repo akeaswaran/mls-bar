2756edf4174effc28d5129a50db5b3c2aed2c8ef
Akshay Easwaran <akeaswaran@me.com>
2018-05-31 20:25:22 -0700

    added build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script builds Donovan for distribution.
+# It first builds the app and then creates two
+# ZIP files which it places on the Desktop. Those
+# files can then all be uploaded to the web.
+
+# Get the bundle version from the plist.
+PLIST_FILE="mls-bar/Info.plist"
+VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $PLIST_FILE)
+
+# Set up file names and paths.
+BUILD_PATH=$(mktemp -d "$TMPDIR/Donovan.XXXXXX")
+ZIP_NAME="Donovan-$VERSION.zip"
+ZIP_MASTER_PATH="$PWD/Releases"
+ZIP_PATH1="$ZIP_MASTER_PATH/$ZIP_NAME"
+
+# Build Donovan in a temporary build location.
+xcodebuild -workspace mls-bar.xcworkspace -scheme mls-bar -configuration Release -derivedDataPath "$BUILD_PATH" build
+
+# Go into the temporary build directory.
+cd "$BUILD_PATH/Build/Products/Release"
+
+# Compress the app.
+rm -f "$ZIP_PATH1"
+mkdir -p "$ZIP_MASTER_PATH"
+mv mls-bar.app Donovan.app
+zip -r -y "$ZIP_PATH1" Donovan.app
